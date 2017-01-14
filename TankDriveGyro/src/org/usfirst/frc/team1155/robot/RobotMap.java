a5f9345a504c7f5c7dc1f3642bdb6bcede3465ac
Karun Ellango <ellangok@bxscience.edu>
2017-01-14 14:25:29 -0500

    Update RobotMap.java

--- TankDriveGyro/src/org/usfirst/frc/team1155/robot/RobotMap.java
+++ TankDriveGyro/src/org/usfirst/frc/team1155/robot/RobotMap.java
@@ -7,13 +7,8 @@ package org.usfirst.frc.team1155.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	public static final int PORT_LEFT_MOTOR_FRONT = 0;
+    public static final int PORT_LEFT_MOTOR_BACK = 2;
+    public static final int PORT_RIGHT_MOTOR_FRONT = 1;
+    public static final int PORT_RIGHT_MOTOR_BACK = 3;
 }
